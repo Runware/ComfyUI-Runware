a5b3ff8cd72a1a53c550ce7fdeec2069e703f4d1
Sirshendu Ganguly <sirshendu.ganguly2015@gmail.com>
2026-04-24 20:02:21 +0530

    Added support for Kling o3 4k models

--- clientlibs/utils.js
+++ clientlibs/utils.js
@@ -2756,6 +2756,7 @@ function videoModelSearchFilterHandler(videoModelSearchNode) {
             "klingai:kling-video@2.6-pro (Kling VIDEO 2.6 Pro)",
             "klingai:kling-video@3-pro (Kling VIDEO 3.0 Pro)",
             "klingai:kling-video@3-standard (Kling VIDEO 3.0 Standard)",
+            "klingai:kling-video@3-4k (Kling VIDEO 3.0 4K)",
             "klingai:kling-video@o3-pro (Kling VIDEO O3 Pro)",
             "klingai:kling-video@o3-standard (Kling VIDEO O3 Standard)",
             "klingai:avatar@2.0-standard (KlingAI Avatar 2.0 Standard)",
@@ -2871,6 +2872,7 @@ function videoModelSearchFilterHandler(videoModelSearchNode) {
         "klingai:kling-video@2.6-pro": {"width": 1920, "height": 1080},
         "klingai:kling-video@3-pro": {"width": 1920, "height": 1080},
         "klingai:kling-video@3-standard": {"width": 1920, "height": 1080},
+        "klingai:kling-video@3-4k": {"width": 3840, "height": 2160},
         "klingai:kling-video@o3-pro": {"width": 1920, "height": 1080},
         "klingai:kling-video@o3-standard": {"width": 1920, "height": 1080},
         "klingai:avatar@2.0-standard": {"width": 0, "height": 0},
@@ -2964,6 +2966,7 @@ function videoModelSearchFilterHandler(videoModelSearchNode) {
         "klingai:kling-video@2.6-pro": "1080p",
         "klingai:kling-video@3-pro": "1080p",
         "klingai:kling-video@3-standard": "1080p",
+        "klingai:kling-video@3-4k": null,  // No 4K option in current resolution dropdown
         "klingai:kling-video@o3-pro": "1080p",
         "klingai:kling-video@o3-standard": "1080p",
         "klingai:avatar@2.0-standard": null,  // No resolution support
